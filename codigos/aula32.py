e8828c7c582f3ddc268fa388d36cbfb5be3ba3b8
alvarobenevides <alvarobenevides5@gmail.com>
2023-12-27 15:42:30 -0300

    feat:Adicionando conteúdo das aulas 31-32

---
+++ codigos/aula32.py
@@ -0,0 +1,72 @@
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+# entrada = input('Digite um número: ')
+
+# if entrada.isdigit():
+#     entrada_int = int(entrada)
+#     par_impar = entrada_int % 2 == 0
+#     par_impar_texto = 'ímpar'
+
+#     if par_impar:
+#         par_impar_texto = 'par'
+
+#     print(f'O número {entrada_int} é {par_impar_texto}')
+# else:
+#     print('Você não digitou um número inteiro')
+
+# try:
+#     entrada_int = int(entrada)
+#     par_impar = entrada_int % 2 == 0
+#     par_impar_texto = 'ímpar'
+
+#     if par_impar:
+#         par_impar_texto = 'par'
+
+#     print(f'O número {entrada_int} é {par_impar_texto}')
+# except:
+#     print('Você não digitou um número inteiro')
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
+# entrada = input('Qual é a hora? ')
+
+# try:
+#     hora = int(entrada)
+
+#     if hora >= 0 and hora <= 11:
+#         print('Bom dia')
+
+#     elif hora >= 12 and hora <= 17:
+#         print('Boa tarde')
+    
+#     elif hora >= 18 and hora <= 23:
+#         print('Boa noite')
+    
+#     else:
+#         print('Não conheço essa hora')
+
+# except:
+#     print('Por favor, digite apenas números inteiros')
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+"""
+# nome = input('Digite o seu nome: ')
+
+# if nome:
+#     quant = len(nome)
+    
+#     if quant <= 4:
+#         print(f'Seu nome {nome} é curto')
+#     if quant >= 5 and quant <= 6:
+#         print(f'Seu nome {nome} é normal')
+#     if quant > 6:
+#         print(f'Seu nome {nome} é muito grande!')
+# else:
+#     print('Escreva seu nome e tente novamente')
